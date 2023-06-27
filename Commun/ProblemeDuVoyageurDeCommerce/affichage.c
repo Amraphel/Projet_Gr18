582c709984993e8f993d4e1ed984c838203cf923
ElisaMrch <elisamarchand@orange.fr>
2023-06-27 16:29:53 +0200

    indentation

--- Commun/ProblemeDuVoyageurDeCommerce/affichage.c
+++ Commun/ProblemeDuVoyageurDeCommerce/affichage.c
@@ -1,10 +1,8 @@
 #include "affichage.h"
 
-
-
-void end_sdl(char ok,         
-             char const *msg,    
-             SDL_Window *window, 
+void end_sdl(char ok,
+             char const *msg,
+             SDL_Window *window,
              SDL_Renderer *renderer)
 {
     char msg_formated[255];
@@ -20,52 +18,50 @@ void end_sdl(char ok,
     }
 
     if (renderer != NULL)
-    {                                
+    {
         SDL_DestroyRenderer(renderer);
         renderer = NULL;
     }
     if (window != NULL)
-    {                   
-        SDL_DestroyWindow(window); 
+    {
+        SDL_DestroyWindow(window);
         window = NULL;
     }
 
     SDL_Quit();
 
     if (!ok)
-    { 
+    {
         exit(EXIT_FAILURE);
     }
 }
 
-
-SDL_Window * initWindow(int x, int y, int w, int h){
+SDL_Window *initWindow(int x, int y, int w, int h)
+{
     SDL_Window *window = NULL;
-    
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         SDL_Log("Error : SDL initialisation - %s\n",
-                SDL_GetError()); 
+                SDL_GetError());
         exit(EXIT_FAILURE);
     }
-     window = SDL_CreateWindow(
-      "Fenêtre 1",          
-      x, y,                 
-      w, h,               
-      SDL_WINDOW_RESIZABLE); 
+    window = SDL_CreateWindow(
+        "Fenêtre",
+        x, y,
+        w, h,
+        SDL_WINDOW_RESIZABLE);
 
-  if (window == NULL)
-  {
-    SDL_Log("Error : SDL window 1 creation - %s\n",
-            SDL_GetError()); // échec de la création de la fenêtre
-    SDL_Quit();              // On referme la SDL
-    exit(EXIT_FAILURE);
-  }
+    if (window == NULL)
+    {
+        SDL_Log("Error : SDL window creation - %s\n",
+                SDL_GetError()); // échec de la création de la fenêtre
+        SDL_Quit();              // On referme la SDL
+        exit(EXIT_FAILURE);
+    }
 
-  return window;
+    return window;
 }
-
 
 // SDL_Rect* drawPoints(SDL_Renderer* renderer, cell_t* listeNoeud, int nombreDePoint){
 //     SDL_Rect tabPoint[nombreDePoint];
