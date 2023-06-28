b2565e8a48b294cb8ed81d55f7af8b7d047864c6
ElisaMrch <elisamarchand@orange.fr>
2023-06-28 09:42:41 +0200

    arret prog avec touche reparer

--- Commun/ProblemeDuVoyageurDeCommerce/main.c
+++ Commun/ProblemeDuVoyageurDeCommerce/main.c
@@ -1,9 +1,9 @@
 #include "affichage.h"
 #include <time.h>
 
-#define P 1
-#define WINDOWL 1000
-#define WINDOWW 1000
+#define P 0.2
+#define WINDOWL 700
+#define WINDOWW 700
 
 int main(int argc, char **argv)
 {
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
     time_t t;
     time(&t);
     srand(t);
-    SDL_Window* window = initWindow(200,200,WINDOWW,WINDOWL);
+    SDL_Window* window = initWindow(200,100,WINDOWW,WINDOWL);
     SDL_Renderer* renderer = initRenderer(window);
     point_t *tabPoint = NULL;
     tabPoint = tabPointAleatoire(NB, window);
@@ -37,8 +37,36 @@ int main(int argc, char **argv)
     generer(mat,0,NB-1);
     genererGraphe(mat,P,NB);
     cell_t* graphe = matToGraphe(mat, NB, tabPoint);
-    drawGraphe(renderer,tabRect,graphe,NB);
-    SDL_Delay(3000);
+    drawGraphe(renderer, tabRect, graphe, NB);
+
+    while (program_on)
+    { 
+        event_utile = SDL_FALSE;
+        while (!event_utile && SDL_PollEvent(&event))
+        {   
+            switch (event.type)
+            {                           
+            case SDL_QUIT:              
+                program_on = SDL_FALSE; 
+                event_utile = SDL_TRUE;
+                break;
+            case SDL_KEYDOWN: 
+                switch (event.key.keysym.sym)
+                {                     
+                case SDLK_ESCAPE:   
+                case SDLK_q:       
+                    program_on = 0; 
+                    event_utile = SDL_TRUE;
+                    break;
+                default: 
+                    break;
+                }
+                break;
+            default: 
+                break;
+            }
+        }
+    }
 
     free(tabPoint);
     free(tabRect);
