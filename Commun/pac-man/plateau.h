497488cbe056cc16eadef994fff2478b6ca34b54
Amraphel <Amraphel@netcourrier.com>
2023-06-29 09:58:41 +0200

    correction erreur

--- Commun/pac-man/plateau.h
+++ Commun/pac-man/plateau.h
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #endif
 
-perso_t* initBlinky(int ** plateau, int w, int h);
+
 int **loadPlateau(char *lvl, int*w, int* h);
 
 void coordPlat(int **plateau,int w, int h, int val, int *x, int *y);
