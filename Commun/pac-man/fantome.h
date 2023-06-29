6e72fc06ae37a610122397682986a0678d20f12b
Amraphel <Amraphel@netcourrier.com>
2023-06-29 09:45:26 +0200

    regalge erreur

--- Commun/pac-man/fantome.h
+++ Commun/pac-man/fantome.h
@@ -3,7 +3,7 @@
 #include "perso.h"
 #endif 
 
-
+perso_t* initBlinky(int ** plateau, int w, int h);
 void moveBlinky(SDL_Texture *my_texture,     //fonction blinky : plus court chemin vers pac-man
                          SDL_Window *window,
-                         SDL_Renderer *renderer)
+                         SDL_Renderer *renderer);
