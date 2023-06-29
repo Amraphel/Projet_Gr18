4fd104c930643d76cae34c3dc3b1c27ff26170b1
Amraphel <Amraphel@netcourrier.com>
2023-06-29 09:11:44 +0200

    maj affichage

--- Commun/pac-man/affichage.h
+++ Commun/pac-man/affichage.h
@@ -2,14 +2,5 @@
 #include "perso.h"
 #include "fantome.h"
 
-void end_sdl(char ok,         
-             char const *msg,    
-             SDL_Window *window, 
-             SDL_Renderer *renderer);
-
-SDL_Window * initWindow(int x, int y, int w, int h);
-SDL_Renderer *initRenderer(SDL_Window *window);
-
 SDL_Rect **createTabRect(SDL_Window *window, int w, int h);
-
 void afficherPlateau(SDL_Rect **tabRect, int **plateau, int w, int h, SDL_Window *window, SDL_Renderer *renderer);
--- Commun/pac-man/perso.h
+++ Commun/pac-man/perso.h
@@ -1,5 +1,22 @@
-#include <SDL2/SDL_image.h> 
-#include <stdio.h>   
+#ifndef BASE
+#define BASE
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <SDL2/SDL_image.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#endif
+
 void play_with_texture_4(SDL_Texture *my_texture,
                          SDL_Window *window,
                          SDL_Renderer *renderer);
+
+
+
+typedef struct perso
+{
+    int id;
+    int posX;
+    int posY;
+};
