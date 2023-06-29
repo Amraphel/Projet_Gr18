5a3dcda6bdec079d192eb6e45a44397ba32c8725
Simon COHU <cohu.simon@gmail.com>
2023-06-29 14:00:16 +0200

    perso.c affichage du perso sur la case voulue

--- Commun/pac-man/perso.c
+++ Commun/pac-man/perso.c
@@ -7,9 +7,7 @@ perso_t* initPac_man(int ** plateau){
 }
 
 
-void mouv_Pac_man(SDL_Texture *my_texture,
-                         SDL_Window *window,
-                         SDL_Renderer *renderer)
+void mouv_Pac_man(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer, int direction, int *posX, int *posY)
 {
     SDL_Rect
         source = {0},            
@@ -20,78 +18,28 @@ void mouv_Pac_man(SDL_Texture *my_texture,
     SDL_GetWindowSize(window, 
                       &window_dimensions.w,
                       &window_dimensions.h);
-    SDL_QueryTexture(my_texture, 
-                     NULL, NULL,
-                     &source.w, &source.h);
+    SDL_QueryTexture(my_texture, NULL, NULL, &source.w, &source.h);
 
     
 
     int nb_images = 4;
-    float zoom = 1;                      
+    float zoom = 1;      
     int offset_x = source.w / nb_images, 
-        offset_y = source.h;             
+        offset_y = source.h;     
+    /*        
     SDL_Event event;
     state.x = 0;        
     state.y = 0;        
     state.w = offset_x; 
     state.h = offset_y; 
-
+    state.x += offset_x; 
+    state.x %= source.w; 
+    */
     destination.w = offset_x * zoom; 
     destination.h = offset_y * zoom; 
+    destination.x = *posX;
+    destination.y = *posY;
 
-    destination.y = 
-        (window_dimensions.h - destination.h) / 2;
-    
-    while (plateau[&Pac_man->posx][]) //à modifier: tant que pac-man est vivant et qu'il reste des pac-gommes
-    {
-        
-        SDL_WaitEvent(&event);
-        switch(event.type)
-        {
-            case SDL_KEYDOWN:
-            switch(event.key.keysym.sym)
-            {
-                case SDLK_UP:     
-                if(plateau[&Pac_man->posx-1][&Pac_man->posy]==0 || plateau[&Pac_man->posx-1][&Pac_man->posy]==-1 || plateau[&Pac_man->posx-1][&Pac_man->posy]>=200)
-                {
-                    SDL_RenderClear(renderer);  
-                    destination.x=destination.x-speed;
-                    state.x += offset_x; 
-                    state.x %= source.w; 
-                    SDL_RenderPresent(renderer);
-                }    
-                break;
-                case SDLK_DOWN:
-                if(plateau[&Pac_man->posx+1][&Pac_man->posy]==0 || plateau[&Pac_man->posx+1][&Pac_man->posy]==-1 || plateau[&Pac_man->posx+1][&Pac_man->posy]>=200)
-                {
-                    SDL_RenderClear(renderer);  
-                    destination.x=destination.x+speed;
-                    state.x += offset_x; 
-                    state.x %= source.w; 
-                    SDL_RenderPresent(renderer);
-                }    
-                break;        
-                case SDLK_RIGHT:
-                if(plateau[&Pac_man->posx][&Pac_man->posy-1]==0 || plateau[&Pac_man->posx][&Pac_man->posy-1]==-1 || plateau[&Pac_man->posx][&Pac_man->posy-1]>=200)
-                {
-                    SDL_RenderClear(renderer);  
-                    destination.y=destination.y+speed;
-                    state.x += offset_x; 
-                    state.x %= source.w; 
-                    SDL_RenderPresent(renderer);
-                }    
-                break;        
-                case SDLK_LEFT:
-                if(plateau[&Pac_man->posx][&Pac_man->posy+1]==0 || plateau[&Pac_man->posx][&Pac_man->posy+1]==-1 || plateau[&Pac_man->posx][&Pac_man->posy+1]>=200)
-                {
-                    SDL_RenderClear(renderer);  
-                    state.x += offset_x; 
-                    state.x %= source.w; 
-                    SDL_RenderPresent(renderer);
-                }    
-                break;        
-            }
-            break;
-        }
-    } 
+    movePersoInPlateau(int ** plateau, int* posX, int* posY, 99, direction)         
+    SDL_RenderCopy(renderer, my_texture, &state, &destination); 
 }
