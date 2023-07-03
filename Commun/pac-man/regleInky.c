0f0eef81a18a27d9e1ec7a45da9ca94dcc4ca1c3
ElisaMrch <elisamarchand@orange.fr>
2023-07-03 15:08:15 +0200

    structure regles

--- Commun/pac-man/regleInky.c
+++ Commun/pac-man/regleInky.c
@@ -1,1 +1,44 @@
 #include "regleInky.h"
+
+void modifRegle(regles_t **tabRegle, int nbRegle, int nbContrainte)
+{
+    int numRegle = rand() % nbRegle;
+    int numContrainte = rand() % nbContrainte;
+    int nouvC = 0;
+    switch (numContrainte)
+    {
+    case 0: // case_ t droite de -1 à 3
+    case 1: // case_ t haut
+    case 2: // case_ t gauche
+    case 3: // case_ t bas
+        nouvC = rand() % 5 -1;
+        break;
+
+    case 4: // distance_t distance_fantome
+    case 5: // distance_t distance_pacman
+        nouvC = rand() % 5;
+        break;
+
+    case 6: // cadran_t cadran_pacman
+    case 7: // cadran_t cadran_pacman
+        nouvC = rand() % 4;
+        break;
+
+    case 8: // int priorite
+        nouvC = rand() % 6;
+        break;
+
+    case 9: // action_t action
+        nouvC = rand() % 4;
+        break;
+
+    default:
+        break;
+    }
+    tabRegle[numRegle][numContrainte] = nouvC;
+}
+
+void initCervau(int nbRegle, int nbContrainte)
+{
+
+}
--- Commun/pac-man/regleInky.h
+++ Commun/pac-man/regleInky.h
@@ -34,5 +34,27 @@ typedef enum
     JOKER = -1,
     MUR,
     VIDE,
-    PACMAN
+    PACMAN,
+    FANTOME
 } case_t;
+
+typedef enum
+{
+    DROITE,
+    HAUT,
+    GAUCHE,
+    BAS
+}action_t;
+
+typedef struct
+{
+    case_t droite, haut, gauche, bas;       // le contenu des 4 cases voisines
+    distance_t distance_fantome;           // dans quelle direction se trouve le fantome le plus proche
+    distance_t distance_pacman;           // dans quelle direction se trouve pacman la plus proche
+    cadran_t cadran_fantome;             // dans quel cadran se trouve le fantome le plus proche
+    cadran_t cadran_pacman;             // dans quel cadran se trouve pacman
+    int priorite;                      // de 0 à 5
+    action_t action;                  // quelle action doit être prise
+}regles_t;
+
+void modifRegle(int **tabRegle);
