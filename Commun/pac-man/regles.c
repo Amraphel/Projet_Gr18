e44aa552a6284b69bb62475cc274435ddc095104
Simon COHU <cohu.simon@gmail.com>
2023-07-03 16:17:12 +0200

    modif regles.c

--- Commun/pac-man/regles.c
+++ Commun/pac-man/regles.c
@@ -1,9 +1,11 @@
-int ** createMatRegles(int width, int height)
+#include "regleInky.h"
+
+regles_t ** createMatRegles(int width, int height)
 {
-    int **matRegles = malloc(sizeof(int *) * width);
+    regles_t **matRegles = malloc(sizeof(regles_t *) * width);
     for (int i = 0; i < width; i++)
     {
-        int *ligne = malloc(sizeof(int) * height);
+        int *ligne = malloc(sizeof(regles_t));
         matRegles[i] = ligne;
         for (int j = 0; j < height; j++)
         {
@@ -13,9 +15,9 @@ int ** createMatRegles(int width, int height)
     return matRegles;
 }
 
-int **loadRegles(char *regles)
+regles_t **loadRegles(char *regles)
 {
-    int **matRegles = NULL;
+    regles_t **matRegles = NULL;
     FILE *file = fopen(regles, "r");
     int* w, h;
     if (file)
@@ -24,15 +26,25 @@ int **loadRegles(char *regles)
 
         matRegles = createMatRegles(*w, *h);
         for (int i = 0; i < *w; i++)
-        {
-            for (int j = 0; j < *h; j++)
-            {
-                fscanf(file, "%d ", &matRegles[i][j]);
-            }
-            fscanf(file, "\n");
+        {            
+            fscanf(file, "%d %d %d %d %d %d %d %d %d %d \n", regles_t[i].droite, regles_t[i].gauche, regles_t[i].haut, regles_t[i].bas, regles_t[i].distance_fantome, regles_t[i].distance_pacman, regles_t[i].cadran_fantome, regles_t[i].cadran_pacman, regles_t[i].priorite, regles_t[i].action);
+
         }
-        // printPlateau(plateau, *w, *h);
     }
     fclose(file);
     return matRegles;
+}
+
+void loadRegles(regles_t ** matRegles, char * regles)
+{
+    FILE *file = fopen(regles, "w");
+    int* w, h;
+    if (file)
+    {
+        for (int i = 0; i < *w; i++)
+        {            
+            fprintf(file, "%d %d %d %d %d %d %d %d %d %d \n", matRegles[i].droite, matRegles[i].gauche, matRegles[i].haut, matRegles[i].bas, matRegles[i].distance_fantome, matRegles[i].distance_pacman, matRegles[i].cadran_fantome, matRegles[i].cadran_pacman, matRegles[i].priorite, matRegles[i].action);
+        }
+    }
+    fclose(file);
 }
