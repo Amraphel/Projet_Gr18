a0d6b2515699c82f6c682d5660da6c8698652553
Simon COHU <cohu.simon@gmail.com>
2023-07-06 11:06:44 +0200

    recup code

--- Commun/pac-man/fantome.c
+++ Commun/pac-man/fantome.c
@@ -115,16 +115,6 @@ void creerHeuri(int **heuri, int **plateau, int x, int y, int val)
             }
         }
     }
-    /*
-    if (heuri[11][23]<heuri[11][-1])
-    {
-        heuri[11][-1]=heuri[11][23];
-    }
-    if (heuri[11][-1]<heuri[11][23])
-    {
-        heuri[11][23]=heuri[11][-1];
-    }
-    */
 }
 
 /**
